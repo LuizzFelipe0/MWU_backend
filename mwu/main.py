6d922db59279fb426ec230b6eadc494afc471ded
Luiz Felipe <102326788+LuizzFelipe0@users.noreply.github.com>
2025-04-21 19:48:12 -0300

    Remove old files and Add new apps at main.py

--- mwu/main.py
+++ mwu/main.py
@@ -1,12 +1,23 @@
+import uvicorn
 from fastapi import FastAPI
 
+from accounts.controller import accounts_router
 from categories.controller import categories_router
-from expenses.controller import expenses_router
+from financial_goals.controller import financial_goals_router
+from transactions.controller import transactions_router
+from user.controller import users_router
 
 app = FastAPI(title="MWU (Money With You)",
-              description="Application to help families manage their user expenses. "
+              description="Application to help families manage their user transactions. "
                           "The application allows you to import receipts and monthly bills (water, electricity, gas),"
-                          "automatically categorize these expenses, and generate monthly and annual expense reports.")
+                          "automatically categorize these transactions, and generate monthly and annual expense reports.")
 
+app.include_router(accounts_router)
 app.include_router(categories_router)
-app.include_router(expenses_router)
+app.include_router(transactions_router)
+app.include_router(financial_goals_router)
+app.include_router(users_router)
+
+
+if __name__ == "__main__":
+    uvicorn.run("mwu.main:app", host="127.0.0.1", port=8000, reload=True)
