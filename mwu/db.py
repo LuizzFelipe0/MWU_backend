f1278d6b0a744220803fca3204f4b3c1e3816ba9
Luiz Felipe <102326788+LuizzFelipe0@users.noreply.github.com>
2025-04-21 19:23:07 -0300

    Remodel db file to consume from MySQL database

--- mwu/db.py
+++ mwu/db.py
@@ -1,13 +1,25 @@
+import os
+
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 
+load_dotenv()
 
-engine = create_engine(
-    'sqlite:///mwu.db', connect_args={"timeout": 30}, echo=True
-)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine, future=True)
+# Database Credentials
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
 
+DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+engine = create_engine(DATABASE_URL, echo=True)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Base for the models
 Base = declarative_base()
 
 
