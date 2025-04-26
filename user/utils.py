6e53558260440c278965718550eaf027f0bd7eec
Luiz Felipe <102326788+LuizzFelipe0@users.noreply.github.com>
2025-04-26 19:44:55 -0300

    Add cpf validator function

---
+++ user/utils.py
@@ -0,0 +1,34 @@
+import re
+
+
+def cpf_validator(cpf: str) -> str:
+    # Remove everything that is not a number
+    cpf = re.sub(r'\D', '', cpf)
+
+    # CPF has to be 11 digits
+    if len(cpf) != 11:
+        return False
+
+    # Repeated sequences like 11111111111 are not allowed
+    if cpf == cpf[0] * 11:
+        return False
+
+    # Validation of first digit
+    cpf_sum = sum(int(cpf[i]) * (10 - i) for i in range(9))
+    first_digit = (cpf_sum * 10) % 11
+    if first_digit == 10:
+        first_digit = 0
+
+    if first_digit != int(cpf[9]):
+        return False
+
+    # Validation of second digit
+    cpf_sum = sum(int(cpf[i]) * (11 - i) for i in range(10))
+    second_digit = (cpf_sum * 10) % 11
+    if second_digit == 10:
+        second_digit = 0
+
+    if second_digit != int(cpf[10]):
+        return False
+
+    return cpf
