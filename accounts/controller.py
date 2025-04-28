45325859dbd34c69d4c06ed8e5d896ca71468441
Luiz Felipe <102326788+LuizzFelipe0@users.noreply.github.com>
2025-04-27 21:45:36 -0300

    Add first version of create account controller

--- accounts/controller.py
+++ accounts/controller.py
@@ -4,6 +4,7 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
 from mwu.db import get_db
+from user.models import User as UserModel, UsersAccounts as UserAccountM2MModel
 from .models import Accounts as AccountModel
 from .schemas import AccountOutput as AccountOutScheme, AccountInput as AccountInScheme
 
@@ -20,19 +21,36 @@ def get_all_accounts(db: Session = Depends(get_db)) -> list[AccountOutScheme | N
 def get_account_by_id(account_id: UUID, db: Session = Depends(get_db)) -> list[AccountOutScheme | None]:
     account = db.query(AccountModel).filter(AccountModel.id == account_id).first()
     if account is None:
-        return HTTPException(status_code=404, detail="Account not found with the given id.")
+        raise HTTPException(status_code=404, detail="Account not found with the given id.")
     return account
 
-"""""
+
 @accounts_router.post("", status_code=201, response_model=AccountOutScheme)
-def create_account(data: AccountInScheme, db: Session = Depends(get_db)) -> AccountOutScheme:
+def create_account(user_id: UUID, data: AccountInScheme, db: Session = Depends(get_db)) -> AccountOutScheme:
+    user = (db.query(UserModel)
+            .filter(UserModel.id == user_id, UserModel.deleted_at.is_(None))
+            .first())
+
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found with the given id.")
+
     account = AccountModel(
+        account_number=data.account_number,
+        balance=data.balance,
         name=data.name,
-        description=data.description
+        type=data.type
     )
     db.add(account)
+    db.flush()  # flush function saves the objects temporary into memory
+
+    user_account = UserAccountM2MModel(
+        user_id=user_id,
+        account_id=account.id
+    )
+    db.add(user_account)
     db.commit()
+
     db.refresh(account)
+    db.refresh(user_account)
 
     return account
-"""
