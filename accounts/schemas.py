50c1b638fc0ba5239415d3485ff15323f9aea654
Luiz Felipe <102326788+LuizzFelipe0@users.noreply.github.com>
2025-04-30 08:17:36 -0300

    Add AccountUpdateInput scheme

--- accounts/schemas.py
+++ accounts/schemas.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from typing import Optional
 from uuid import UUID
 
 from pydantic import BaseModel
@@ -9,6 +10,13 @@ class AccountInput(BaseModel):
     type: str
     account_number: str
     balance: float
+
+
+class AccountUpdateInput(BaseModel):
+    name: Optional[str] = None
+    type: Optional[str] = None
+    account_number: Optional[str] = None
+    balance: Optional[float] = None
 
 
 class AccountOutput(BaseModel):
