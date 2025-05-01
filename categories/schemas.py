2265fd9dd0cfb2a25089abb7c5aeb5ab9dd39627
Luiz Felipe <102326788+LuizzFelipe0@users.noreply.github.com>
2025-05-01 20:50:23 -0300

    Update type column at categories schemas

--- categories/schemas.py
+++ categories/schemas.py
@@ -1,22 +1,30 @@
 from datetime import datetime
+from enum import Enum
 from typing import Optional
 from uuid import UUID
 
 from pydantic import BaseModel
 
 
+class TypeEnum(str, Enum):
+    Income = "Income"
+    Expense = "Expense"
+    Transfer = "Transfer"
+    Investment = "Investment"
+
+
 class CategoryInput(BaseModel):
     user_id: UUID
     name: str
     description: str
-    type: str
+    type: TypeEnum | None
 
 
 class CategoryUpdateInput(BaseModel):
     user_id: Optional[UUID] = None
     name: Optional[str] = None
     description: Optional[str] = None
-    type: Optional[str] = None
+    type: TypeEnum = None
 
 
 class CategoryOutput(BaseModel):
@@ -24,7 +32,7 @@ class CategoryOutput(BaseModel):
     user_id: UUID
     name: str
     description: str
-    type: str
+    type: TypeEnum | None
     created_at: datetime
     updated_at: datetime
     deleted_at: datetime | None
