798d43e66dba77dce9e1a544b0aa6fd17fd25b36
Luiz Felipe <102326788+LuizzFelipe0@users.noreply.github.com>
2025-04-30 23:14:17 -0300

    Remove UserAccounts logic from Account post controller

--- accounts/controller.py
+++ accounts/controller.py
@@ -5,7 +5,6 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
 from mwu.db import get_db
-from user.models import User as UserModel, UsersAccounts as UserAccountM2MModel
 from .models import Accounts as AccountModel
 from .schemas import AccountOutput as AccountOutScheme, AccountInput as AccountInScheme, \
     AccountUpdateInput as AccountUpdateInScheme
@@ -29,14 +28,7 @@ def get_account_by_id(account_id: UUID, db: Session = Depends(get_db)) -> list[A
 
 
 @accounts_router.post("", status_code=201, response_model=AccountOutScheme)
-def create_account(user_id: UUID, data: AccountInScheme, db: Session = Depends(get_db)) -> AccountOutScheme:
-    user = (db.query(UserModel)
-            .filter(UserModel.id == user_id, UserModel.deleted_at.is_(None))
-            .first())
-
-    if not user:
-        raise HTTPException(status_code=404, detail="User not found with the given id.")
-
+def create_account(data: AccountInScheme, db: Session = Depends(get_db)) -> AccountOutScheme:
     account = AccountModel(
         account_number=data.account_number,
         balance=data.balance,
@@ -44,17 +36,9 @@ def create_account(user_id: UUID, data: AccountInScheme, db: Session = Depends(g
         type=data.type
     )
     db.add(account)
-    db.flush()  # flush function saves the objects temporary into memory
 
-    user_account = UserAccountM2MModel(
-        user_id=user_id,
-        account_id=account.id
-    )
-    db.add(user_account)
     db.commit()
-
     db.refresh(account)
-    db.refresh(user_account)
 
     return account
 
