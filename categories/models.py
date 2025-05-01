eb06e5b17b426b9d104a3b613b18c310916a51ea
Luiz Felipe <102326788+LuizzFelipe0@users.noreply.github.com>
2025-05-01 20:47:30 -0300

    Update type to use TypeEnum at category model

--- categories/models.py
+++ categories/models.py
@@ -1,9 +1,17 @@
+import enum
 from datetime import datetime
 
-from sqlalchemy import Column, String, DateTime, Uuid
+from sqlalchemy import Column, String, DateTime, Uuid, Enum
 from uuid_extensions import uuid7
 
 from mwu.db import Base as _Base
+
+
+class TypeEnum(enum.Enum):
+    Income = 'Income'
+    Expense = 'Expense'
+    Transfer = 'Transfer'
+    Investment = 'Investment'
 
 
 class Category(_Base):
@@ -13,7 +21,7 @@ class Category(_Base):
     user_id = Column(Uuid, primary_key=True, default=uuid7)
     name = Column(String, nullable=False)
     description = Column(String, nullable=True)
-    type = Column(String, nullable=False)
+    type = Column(Enum(TypeEnum, name="typeenum"), nullable=False)
     created_at = Column(DateTime, nullable=False, default=datetime.now())
     updated_at = Column(DateTime, nullable=False, default=datetime.now())
     deleted_at = Column(DateTime, nullable=True)
