a7031aab3a3ed0a7e5b0a5244fa988d86ede2a1b
Luiz Felipe <102326788+LuizzFelipe0@users.noreply.github.com>
2025-05-03 16:17:53 -0300

    Remove type column and add category_type_id foreign key column referencing the table category_type

--- categories/models.py
+++ categories/models.py
@@ -1,17 +1,9 @@
-import enum
 from datetime import datetime
 
-from sqlalchemy import Column, String, DateTime, Uuid, Enum
+from sqlalchemy import Column, String, DateTime, Uuid
 from uuid_extensions import uuid7
 
 from mwu.db import Base as _Base
-
-
-class TypeEnum(enum.Enum):
-    Income = 'Income'
-    Expense = 'Expense'
-    Transfer = 'Transfer'
-    Investment = 'Investment'
 
 
 class Category(_Base):
@@ -19,9 +11,9 @@ class Category(_Base):
 
     id = Column(Uuid, primary_key=True, default=uuid7)
     user_id = Column(Uuid, primary_key=True, default=uuid7)
+    category_type_id = Column(Uuid, primary_key=True, default=uuid7)
     name = Column(String, nullable=False)
     description = Column(String, nullable=True)
-    type = Column(Enum(TypeEnum, name="typeenum"), nullable=False)
     created_at = Column(DateTime, nullable=False, default=datetime.now())
     updated_at = Column(DateTime, nullable=False, default=datetime.now())
     deleted_at = Column(DateTime, nullable=True)
