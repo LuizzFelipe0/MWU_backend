e52b7c7db3e0570736ef9de93d34dadf87c3c8d5
Luiz Felipe <102326788+LuizzFelipe0@users.noreply.github.com>
2025-05-03 16:18:10 -0300

    Remove type column and add category_type_id at schemas

--- categories/schemas.py
+++ categories/schemas.py
@@ -1,38 +1,30 @@
 from datetime import datetime
-from enum import Enum
 from typing import Optional
 from uuid import UUID
 
 from pydantic import BaseModel
 
 
-class TypeEnum(str, Enum):
-    Income = "Income"
-    Expense = "Expense"
-    Transfer = "Transfer"
-    Investment = "Investment"
-
-
 class CategoryInput(BaseModel):
     user_id: UUID
+    category_type_id: UUID
     name: str
     description: str
-    type: TypeEnum | None
 
 
 class CategoryUpdateInput(BaseModel):
     user_id: Optional[UUID] = None
+    category_type_id: Optional[UUID] = None
     name: Optional[str] = None
     description: Optional[str] = None
-    type: TypeEnum = None
 
 
 class CategoryOutput(BaseModel):
     id: UUID
     user_id: UUID
+    category_type_id: UUID
     name: str
     description: str
-    type: TypeEnum | None
     created_at: datetime
     updated_at: datetime
     deleted_at: datetime | None
