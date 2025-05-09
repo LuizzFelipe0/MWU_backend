8201de729567bcb920968e0fb9413196d3c08fcc
Luiz Felipe <102326788+LuizzFelipe0@users.noreply.github.com>
2025-05-08 23:35:27 -0300

    Change account_id and next_due_dates schema validation for null values

--- transactions/schemas.py
+++ transactions/schemas.py
@@ -16,16 +16,27 @@ class TransactionInput(BaseModel):
     next_due_date: Optional[datetime] = None
 
 
+class TransactionUpdateInput(BaseModel):
+    user_id: Optional[UUID] = None
+    account_id: Optional[UUID] = None
+    category_id: Optional[UUID] = None
+    name: Optional[str] = None
+    amount: Optional[float] = None
+    date: Optional[datetime] = None
+    is_recurring: Optional[bool] = None
+    next_due_date: Optional[datetime] = None
+
+
 class TransactionOutput(BaseModel):
     id: UUID
     user_id: UUID
-    account_id: UUID
+    account_id: Optional[UUID] = None
     category_id: UUID
     name: str
     amount: float
     date: datetime
     is_recurring: bool
-    next_due_date: datetime | None
+    next_due_date: Optional[datetime] = None
     created_at: datetime
     updated_at: datetime
     deleted_at: datetime | None
