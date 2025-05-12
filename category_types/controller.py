3342296b32c1606c76dbc5f9791bf50bcafd764b
Luiz Felipe <102326788+LuizzFelipe0@users.noreply.github.com>
2025-05-11 22:26:01 -0300

    Apply new category_type controller model

--- category_types/controller.py
+++ category_types/controller.py
@@ -15,45 +15,21 @@ class CategoryTypeController:
     service: CategoryTypeRepository = Depends()
 
     @category_types_router.get("/all", response_model=list[CategoryTypesOutScheme])
-    def get_all(self):
-        return self.service.get_all_category_types()
+    def get_all_category_types(self):
+        category_types = self.service.get_all_category_types()
+        return category_types
 
     @category_types_router.post("/create", response_model=CategoryTypesOutScheme, status_code=201)
-    def create(self, data: CategoryTypeInScheme):
-        return self.service.create_category_type(data)
+    def create_category_type(self, data: CategoryTypeInScheme):
+        category_type = self.service.create_category_type(data)
+        return category_type
 
-    @category_types_router.patch("/update/{category_id}", status_code=200)
+    @category_types_router.patch("/{category_id}/update", status_code=200)
     def update_category_type(self, category_id: UUID, data: CategoryTypesUpdateInScheme) -> CategoryTypesUpdateInScheme:
-        return self.service.update_category_type(id=category_id, data=data)
+        category_type = self.service.update_category_type(id=category_id, data=data)
+        return category_type
 
-
-"""
-
-
-@category_types_router.patch("/update", status_code=200)
-def update_category_type(category_type_id: UUID, data: CategoryTypesUpdateInScheme,
-                         db: Session = Depends(get_db)) -> CategoryTypesUpdateInScheme:
-    category_type = db.query(CategoryTypesModel).filter_by(id=category_type_id).first()
-
-    if not category_type:
-        raise HTTPException(status_code=404, detail="Category Type not found")
-
-    update_data = data.dict(exclude_unset=True)
-
-    for key, value in update_data.items():
-        setattr(category_type, key, value)
-
-    db.commit()
-
-    return category_type
-
-
-@category_types_router.delete("/delete", status_code=204)
-def delete_category_type(category_type_id: UUID, db: Session = Depends(get_db)):
-    category_type = db.query(CategoryTypesModel).filter_by(id=category_type_id).first()
-
-    if not category_type:
-        raise HTTPException(status_code=404, detail="Category Type not found")
-
-    db.delete(category_type)
-    db.commit()"""
+    @category_types_router.delete("/{category_id}/delete", status_code=204)
+    def delete_category_type(self, category_id: UUID):
+        category_type = self.service.delete_category_type(id=category_id)
+        return category_type
