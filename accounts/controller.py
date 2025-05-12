ce8724ec3e35db64d23d69405344a919011c4e97
Luiz Felipe <102326788+LuizzFelipe0@users.noreply.github.com>
2025-05-11 22:51:57 -0300

    Remodel accounts controllers to use cbv

--- accounts/controller.py
+++ accounts/controller.py
@@ -1,102 +1,50 @@
-from datetime import datetime
 from uuid import UUID
 
-from fastapi import APIRouter, Depends, HTTPException
-from sqlalchemy.orm import Session
+from fastapi import APIRouter, Depends
+from fastapi_utils.cbv import cbv
 
-from mwu.db import get_db
-from .models import Accounts as AccountModel
+from .repository import AccountRepository
 from .schemas import AccountOutput as AccountOutScheme, AccountInput as AccountInScheme, \
     AccountUpdateInput as AccountUpdateInScheme
 
 accounts_router = APIRouter(prefix="/accounts", tags=["Accounts"])
 
 
-@accounts_router.get("")
-def get_all_accounts(db: Session = Depends(get_db)) -> list[AccountOutScheme | None]:
-    accounts = db.query(AccountModel).filter(AccountModel.deleted_at.is_(None)).all()
-    return accounts
+@cbv(accounts_router)
+class AccountController:
+    service: AccountRepository = Depends()
 
+    @accounts_router.get("/all", response_model=list[AccountOutScheme | None])
+    def get_all_accounts(self):
+        accounts = self.service.get_all_accounts()
+        return accounts
 
-@accounts_router.get("/deleted")
-def get_deleted_accounts(db: Session = Depends(get_db)) -> list[AccountOutScheme | None]:
-    deleted_accounts = db.query(AccountModel).filter(AccountModel.deleted_at.isnot(None)).all()
-    return deleted_accounts
+    @accounts_router.get("/deleted", response_model=list[AccountOutScheme | None])
+    def get_deleted_accounts(self):
+        deleted_accounts = self.service.get_deleted_accounts()
+        return deleted_accounts
 
+    @accounts_router.get("/{account_id}")
+    def get_account_by_id(self, account_id: UUID) -> AccountOutScheme | None:
+        account = self.service.get_account_by_id(id=account_id)
+        return account
 
-@accounts_router.get("/{account_id}")
-def get_account_by_id(account_id: UUID, db: Session = Depends(get_db)) -> list[AccountOutScheme | None]:
-    account = (db.query(AccountModel).
-               filter(AccountModel.id == account_id, AccountModel.deleted_at.is_(None)))
-    if account is None:
-        raise HTTPException(status_code=404, detail="Account not found with the given id.")
-    return account
+    @accounts_router.post("/create", response_model=AccountOutScheme, status_code=201)
+    def create_account(self, data: AccountInScheme) -> AccountOutScheme:
+        account = self.service.create_account(data)
+        return account
 
+    @accounts_router.patch("/{account_id}/update", status_code=200)
+    def update_account(self, account_id: UUID, data: AccountUpdateInScheme) -> AccountOutScheme:
+        account = self.service.update_account(id=account_id, data=data)
+        return account
 
-@accounts_router.post("", status_code=201, response_model=AccountOutScheme)
-def create_account(data: AccountInScheme, db: Session = Depends(get_db)) -> AccountOutScheme:
-    account = AccountModel(
-        account_number=data.account_number,
-        balance=data.balance,
-        name=data.name,
-        type=data.type
-    )
-    db.add(account)
+    @accounts_router.delete("/{account_id}/delete", status_code=200)
+    def delete_account(self, account_id: UUID) -> AccountOutScheme:
+        account = self.service.delete_account(id=account_id)
+        return account
 
-    db.commit()
-    db.refresh(account)
-
-    return account
-
-
-@accounts_router.post("/{account_id}/restore", status_code=200, response_model=AccountOutScheme)
-def restore_deleted_account(account_id: UUID, db: Session = Depends(get_db)) -> AccountOutScheme:
-    deleted_account = (
-        db.query(AccountModel)
-        .filter(
-            AccountModel.id == account_id,
-            AccountModel.deleted_at.isnot(None)).first()
-    )
-
-    if not deleted_account:
-        raise HTTPException(status_code=404, detail="This Account is not deleted or was not found in the database!")
-
-    deleted_account.updated_at = datetime.now()
-    deleted_account.deleted_at = None
-    db.commit()
-    db.refresh(deleted_account)
-
-    return deleted_account
-
-
-@accounts_router.patch("/update/{account_id}", status_code=200, response_model=AccountOutScheme)
-def update_account(account_id: UUID, data: AccountUpdateInScheme,
-                   db: Session = Depends(get_db)) -> AccountUpdateInScheme:
-    account = db.query(AccountModel).filter(AccountModel.id == account_id,
-                                            AccountModel.deleted_at.is_(None)).first()
-
-    if not account:
-        raise HTTPException(status_code=404, detail="Account not found with the given id.")
-
-    update_data = data.dict(exclude_unset=True)
-
-    for key, value in update_data.items():
-        setattr(account, key, value)
-
-    account.updated_at = datetime.now()
-    db.commit()
-
-    return account
-
-
-@accounts_router.delete("/delete/{account_id}", status_code=200, response_model=AccountOutScheme)
-def delete_account(account_id: UUID, db: Session = Depends(get_db)) -> AccountOutScheme:
-    account = db.query(AccountModel).filter(AccountModel.id == account_id,
-                                            AccountModel.deleted_at.is_(None)).first()
-    if not account:
-        raise HTTPException(status_code=404, detail="Account not found with the given id.")
-
-    account.deleted_at = datetime.now()
-    db.commit()
-
-    return account
+    @accounts_router.post("{account_id}/restore", status_code=200)
+    def restore_account(self, account_id: UUID) -> AccountOutScheme:
+        account = self.service.restore_account(id=account_id)
+        return account
