0de0a53afea0ce1802319f1015deacb418c16a9b
Luiz Felipe <102326788+LuizzFelipe0@users.noreply.github.com>
2025-05-11 22:28:39 -0300

    Apply is_strong_password http exception directly at the function

--- user/auth/security.py
+++ user/auth/security.py
@@ -1,5 +1,6 @@
 import re
 
+from fastapi import HTTPException
 from passlib.context import CryptContext
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
@@ -15,10 +16,15 @@ def verify_password(plain_password: str, hashed_password: str) -> bool:
 
 def is_strong_password(password: str) -> bool:
     # Need to have strong password, 1 number, 1 Upper and 1 Lower letter, 1 number and 1 special character
-    return (
+    strong_password = (
             len(password) >= 8
             and bool(re.search(r"[A-Z]", password))
             and bool(re.search(r"[a-z]", password))
             and bool(re.search(r"[0-9]", password))
             and bool(re.search(r"[!@#$%^&*(),.?\":{}|<>]", password))
     )
+
+    if not strong_password:
+        raise HTTPException(status_code=422, detail="This Password is not valid, Try Again!")
+
+    return strong_password
