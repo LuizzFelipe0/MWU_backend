d59b4f981bbb052f660421aff1335d1dc6950b69
Luiz Felipe <102326788+LuizzFelipe0@users.noreply.github.com>
2025-05-18 18:52:12 -0300

    Fix update_user logic at UserRepository

--- user/repository.py
+++ user/repository.py
@@ -3,9 +3,9 @@ from uuid import UUID
 from fastapi import Depends
 from sqlalchemy.orm import Session
 
+from auth.security import is_strong_password, hash_password
 from mwu.db import get_db
 from mwu.services import ModelOperationalService
-from .auth.security import is_strong_password, hash_password
 from .models import User as UserModel
 from .schemas import UserInput as UserInScheme, UserUpdateInput as UserUpdateInScheme
 from .utils import cpf_validator
@@ -41,14 +41,16 @@ class UserRepository(ModelOperationalService):
 
     def update_user(self, id: UUID, data: UserUpdateInScheme):
         user_with_id_validated = self.get_obj_by_id_not_deleted(id)
+        user_data = data.dict(exclude_unset=True)
 
-        if data.password is not None:
-            is_strong_password(data.password)
+        if 'password' in user_data:
+            is_strong_password(user_data['password'])
+            user_data['password'] = hash_password(user_data['password'])
 
-        if data.cpf is not None:
-            cpf_validator(data.cpf)
+        if 'cpf' in user_data:
+            user_data['cpf'] = cpf_validator(user_data['cpf'])
 
-        user = self.update(obj_id=user_with_id_validated.id, data=data)
+        user = self.update(obj_id=user_with_id_validated.id, data=UserUpdateInScheme(**user_data))
         return user
 
     def delete_user(self, id: UUID):
