4d26861ca18006165c3a596284eb6c4d6a237f83
Luiz Felipe <lfcanariocosta04@gmail.com>
2025-08-24 15:10:21 -0300

    Add expenses controller

--- analytics/expenses/controller.py
+++ analytics/expenses/controller.py
@@ -1,9 +1,25 @@
-from fastapi import APIRouter
+from uuid import UUID
 
-analytics_router = APIRouter(prefix="/analytics", tags=["Analytics"])
+from fastapi import APIRouter, Depends
+from fastapi_utils.cbv import cbv
 
-"""@analytics_router.get("/goals")
-def get_all_goals(db: Session = Depends(get_db)) -> list[CategoryOutScheme | None]:
-    categories = db.query(CategoryModel).filter(CategoryModel.deleted_at.is_(None)).all()
-    return categories
-"""
+from analytics.expenses.schemas import (
+    MonthlyCategoryExpense,
+    TotalExpenseCategoryDistribution,
+)
+from analytics.expenses.service import ExpensesService
+
+expenses_router = APIRouter(prefix="/expenses-analysis", tags=["Expenses Analysis"])
+
+
+@cbv(expenses_router)
+class ExpensesController:
+    service: ExpensesService = Depends()
+
+    @expenses_router.get("/expenses/{user_id}/monthly-by-category", response_model=list[MonthlyCategoryExpense])
+    def get_monthly_expenses_by_category_type(self, user_id: UUID):
+        return self.service.get_monthly_expenses_by_category(user_id)
+
+    @expenses_router.get("/expenses/{user_id}/distribution", response_model=list[TotalExpenseCategoryDistribution])
+    def get_total_expense_distribution(self, user_id: UUID):
+        return self.service.get_total_expense_distribution(user_id)
