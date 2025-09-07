c2ebb2129263cb44a7f0c25b201eb0717e41f914
Luiz Felipe <lfcanariocosta04@gmail.com>
2025-09-07 15:02:10 -0300

    Add is_recurring_expense controller param

--- analytics/expenses/controller.py
+++ analytics/expenses/controller.py
@@ -1,6 +1,7 @@
+from typing import Optional
 from uuid import UUID
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Query
 from fastapi_utils.cbv import cbv
 
 from analytics.expenses.schemas import (
@@ -17,9 +18,12 @@ class ExpensesController:
     service: ExpensesService = Depends()
 
     @expenses_router.get("/expenses/{user_id}/monthly-by-category", response_model=list[MonthlyCategoryExpense])
-    def get_monthly_expenses_by_category_type(self, user_id: UUID):
-        return self.service.get_monthly_expenses_by_category(user_id)
-
+    def get_monthly_expenses_by_category_type(
+            self,
+            user_id: UUID,
+            is_recurring_expense: Optional[bool] = Query(None, description="Filter by recurring expenses."),
+    ):
+        return self.service.get_monthly_expenses_by_category(user_id, is_recurring_expense)
     @expenses_router.get("/expenses/{user_id}/distribution", response_model=list[TotalExpenseCategoryDistribution])
     def get_total_expense_distribution(self, user_id: UUID):
         return self.service.get_total_expense_distribution(user_id)
