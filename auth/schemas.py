be2301d17ff5493886c8df4b1c8359297ea8ed6d
Luiz Felipe <lfcanariocosta04@gmail.com>
2026-02-21 17:04:01 -0300

    Add access_token, token_type and UserInfo at auth schemas

--- auth/schemas.py
+++ auth/schemas.py
@@ -1,15 +1,17 @@
-from datetime import datetime
-
+from uuid import UUID
 from pydantic import BaseModel
-
 
 class LoginInput(BaseModel):
     email: str
     password: str
 
-
-class LoginOutput(BaseModel):
+class UserInfo(BaseModel):
+    id: UUID
     email: str
     first_name: str
     cpf: str
-    created_at: datetime
+
+class LoginOutput(BaseModel):
+    access_token: str
+    token_type: str = "bearer"
+    user: UserInfo
