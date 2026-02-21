9a87e671360a94a0e4e32567503a7c2393e6ea5f
Luiz Felipe <lfcanariocosta04@gmail.com>
2026-02-21 17:04:28 -0300

    Add JWT access token functions

--- auth/security.py
+++ auth/security.py
@@ -1,7 +1,14 @@
+import os
 import re
-
+from datetime import datetime, timedelta
+from jose import JWTError, jwt
 from fastapi import HTTPException
 from passlib.context import CryptContext
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = 60 * 24
+
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -12,6 +19,21 @@ def hash_password(password: str) -> str:
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
+
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+
+def decode_access_token(token: str):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        return payload
+    except JWTError:
+        return None
 
 
 def is_strong_password(password: str) -> bool:
