1d6e795aa8152afe878c2674c1f8050b1d15d3c0
Luiz Felipe <lfcanariocosta04@gmail.com>
2026-02-21 17:05:07 -0300

    Update get_current_user to consume from JWT architecture

--- auth/utils.py
+++ auth/utils.py
@@ -1,19 +1,30 @@
-from fastapi import Header, HTTPException, Depends
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.orm import Session
 from uuid import UUID
 
 from mwu.db import get_db
 from user.service import UserService
+from auth.security import decode_access_token
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
 
 
-def get_current_user(x_user_id: str = Header(...), db: Session = Depends(get_db)):
-    try:
-        user_id = UUID(x_user_id)
-    except ValueError:
-        raise HTTPException(status_code=400, detail="Invalid user ID format")
+def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
+    payload = decode_access_token(token)
+    if not payload:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Could not validate credentials",
+        )
+
+    user_id = payload.get("sub")
+
+    if not user_id:
+        raise HTTPException(status_code=401, detail="Invalid token payload")
 
     repo = UserService(session=db)
-    user = repo.get_user_by_id(user_id)
+    user = repo.get_user_by_id(UUID(user_id))
 
     if not user:
         raise HTTPException(status_code=404, detail="User not found")
