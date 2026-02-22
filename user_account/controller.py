a6aa9410b2c9e0e8773b3dfdf81c06fc70e5af07
Luiz Felipe <lfcanariocosta04@gmail.com>
2026-02-22 17:00:09 -0300

    Add authentication parameters and updated service functions at user_accounts controllers

--- user_account/controller.py
+++ user_account/controller.py
@@ -3,6 +3,7 @@ from uuid import UUID
 from fastapi import APIRouter, Depends
 from fastapi_utils.cbv import cbv
 
+from auth.utils import get_current_admin_user, get_current_user
 from .schemas import UserAccountOutput as UserAccountOutScheme
 from .service import UserAccountService
 
@@ -14,32 +15,32 @@ class UserAccountController:
     service: UserAccountService = Depends()
 
     @user_account_router.get("/all", response_model=list[UserAccountOutScheme])
-    def get_all_user_account_relations(self):
+    def get_all_user_account_relations(self, admin = Depends(get_current_admin_user)):
         account_user_relations = self.service.get_all_user_accounts()
         return account_user_relations
 
     @user_account_router.get("/{user_acount_id}", response_model=UserAccountOutScheme)
-    def get_user_accounts_relation_by_id(self,  user_acount_id: UUID):
+    def get_user_accounts_relation_by_id(self,  user_acount_id: UUID, admin = Depends(get_current_admin_user)):
         user_accounts_relation = self.service.get_user_accounts_relation_by_id(user_acount_id)
         return user_accounts_relation
 
     @user_account_router.get("/accounts/{user_id}/users", response_model=list[UserAccountOutScheme])
-    def get_accounts_for_user(self, user_id: UUID):
-        account_user_relation = self.service.get_accounts_for_user_relation(user_id=user_id)
+    def get_accounts_for_user(self, current_user = Depends(get_current_user)):
+        account_user_relation = self.service.get_accounts_for_user_relation(user_id=current_user.id)
         return account_user_relation
 
     @user_account_router.get("/users/{account_id}/accounts", response_model=list[UserAccountOutScheme])
-    def get_users_for_account(self, account_id: UUID):
-        user_account_relation = self.service.get_users_for_account_relation(account_id=account_id)
+    def get_users_for_account(self, account_id: UUID, current_user = Depends(get_current_user)):
+        user_account_relation = self.service.get_users_for_account_relation(account_id=account_id, user_id=current_user.id)
         return user_account_relation
 
     @user_account_router.post("/users/{user_id}/accounts/{account_id}", response_model=UserAccountOutScheme,
                               status_code=201)
-    def create_user_account_relation(self, user_id: UUID, account_id: UUID):
-        user_account_relation = self.service.create_user_account_relation(user_id=user_id, account_id=account_id)
+    def create_user_account_relation(self, account_id: UUID, current_user = Depends(get_current_user)):
+        user_account_relation = self.service.create_user_account_relation(user_id=current_user.id, account_id=account_id)
         return user_account_relation
 
     @user_account_router.delete("/users/{user_id}/accounts/{account_id}", status_code=204)
-    def delete_user_account_relation(self, user_id: UUID, account_id: UUID):
-        user_account_relation = self.service.delete_user_account_relation(user_id=user_id, account_id=account_id)
+    def delete_user_account_relation(self, account_id: UUID, current_user = Depends(get_current_user)):
+        user_account_relation = self.service.delete_user_account_relation(user_id=current_user.id, account_id=account_id)
         return user_account_relation
