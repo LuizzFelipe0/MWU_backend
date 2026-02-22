2ebe15bdca91b8e6e42ef4e2bc3058a34b31de36
Luiz Felipe <lfcanariocosta04@gmail.com>
2026-02-22 12:58:33 -0300

    Remodel category_types service with new Repository

--- category_types/service.py
+++ category_types/service.py
@@ -3,33 +3,32 @@ from uuid import UUID
 from fastapi import Depends
 from sqlalchemy.orm import Session
 
-from category_types.models import CategoryTypes as CategoryTypesModel
 from mwu.db import get_db
-from mwu.repositories.operational_repositories import ModelOperationalRepository
+from .repository import CategoryTypeRepository
 from .schemas import CategoryTypesInput as CategoryTypeInScheme, CategoryTypesUpdateInput as CategoryTypesUpdateInScheme
 
 
-class CategoryTypeService(ModelOperationalRepository):
+class CategoryTypeService:
     def __init__(self, session: Session = Depends(get_db)):
-        super().__init__(model=CategoryTypesModel, session=session)
+        self.category_type_repository = CategoryTypeRepository(session)
 
     def get_all_category_types(self):
-        category_types = self.get_all()
+        category_types = self.category_type_repository.get_all()
         return category_types
 
     def get_category_type_by_id(self, category_type_id: UUID):
-        category_type = self.get_obj_by_id(obj_id=category_type_id)
+        category_type = self.category_type_repository.get_by_id(obj_id=category_type_id)
         return category_type
 
     def create_category_type(self, data: CategoryTypeInScheme):
-        category_type = self.create(data=data)
+        category_type = self.category_type_repository.create(data=data)
         return category_type
 
     def update_category_type(self, id: UUID, data: CategoryTypesUpdateInScheme):
-        category_type_with_id_validated = self.get_obj_by_id(id)
-        category_type = self.update(obj_id=category_type_with_id_validated.id, data=data)
+        category_type_with_id_validated = self.category_type_repository.get_by_id(id)
+        category_type = self.category_type_repository.update(obj_id=category_type_with_id_validated.id, data=data)
         return category_type
 
     def delete_category_type(self, id: UUID):
-        category_type = self.force_delete(obj_id=id)
+        category_type = self.category_type_repository.force_delete(obj_id=id)
         return category_type
