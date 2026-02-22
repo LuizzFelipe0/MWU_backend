6a53fc49d88ac551cfb128f87f8fca2f6c2c314f
Luiz Felipe <lfcanariocosta04@gmail.com>
2026-02-22 17:00:53 -0300

    Remodel user_account service

--- user_account/service.py
+++ user_account/service.py
@@ -3,51 +3,63 @@ from uuid import UUID
 from fastapi import Depends
 from sqlalchemy.orm import Session
 
-from accounts.models import Accounts as AccountModel
+from accounts.repository import AccountRepository
 from mwu.db import get_db
-from mwu.repositories.operational_repositories import ModelRelationRepository, ModelOperationalRepository
-from user.models import User as UserModel
-from .models import UsersAccounts as UserAccountModel
+from user.repository import UserRepository
+from .repository import UserAccountsRepository
 
 
 class UserAccountService:
     def __init__(self, session: Session = Depends(get_db)):
         self.db = session
-        self.relation_repository = ModelRelationRepository(
-            relation_model=UserAccountModel,
-            first_model_key="user_id",
-            second_model_key="account_id",
-            session=session
-        )
-        self.user_repository = ModelOperationalRepository(UserModel, session=session)
-        self.account_repository = ModelOperationalRepository(AccountModel, session=session)
+        self.accounts_repository = AccountRepository(session=session)
+        self.user_repository = UserRepository(session=session)
+        self.user_accounts_repository = UserAccountsRepository(session=session)
 
     def get_all_user_accounts(self):
-        user_accounts = self.relation_repository.get_all_relations()
+        user_accounts = self.user_accounts_repository.get_all()
         return user_accounts
 
     def get_user_accounts_relation_by_id(self, user_account_id: UUID):
-        user_accounts_relation = self.relation_repository.get_relation_by_id(relation_id=user_account_id)
+        user_accounts_relation = self.user_accounts_repository.get_by_id(obj_id=user_account_id)
         return user_accounts_relation
 
     def create_user_account_relation(self, user_id: UUID, account_id: UUID):
-        self.user_repository.get_obj_by_id_not_deleted(user_id)
-        self.account_repository.get_obj_by_id_not_deleted(account_id)
+        self.user_repository.get_by_id(obj_id=user_id)
+        self.accounts_repository.get_by_id(obj_id=account_id)
 
-        create_user_account_relation = self.relation_repository.create_relationship(user_id, account_id)
+        create_user_account_relation = self.user_accounts_repository.create_relation(
+            user_id=user_id,
+            account_id=account_id
+        )
         return create_user_account_relation
 
     def delete_user_account_relation(self, user_id: UUID, account_id: UUID):
-        delete_user_account_relation = self.relation_repository.delete_relationship(user_id, account_id)
+        delete_user_account_relation = self.user_accounts_repository.delete_relation(
+            user_id=user_id,
+            account_id=account_id
+        )
 
         return delete_user_account_relation
 
     def get_accounts_for_user_relation(self, user_id: UUID):
-        accounts_for_users = self.relation_repository.get_relations_by_key("user_id", user_id)
+        accounts_for_users = self.user_accounts_repository.get_relation_by_user(user_id=user_id)
 
         return accounts_for_users
 
-    def get_users_for_account_relation(self, account_id: UUID):
-        users_for_account = self.relation_repository.get_relations_by_key("account_id", account_id)
+    def get_users_for_account_relation(self, account_id: UUID, user_id: UUID):
+        have_access = self.user_accounts_repository.get_relation(
+            user_id=user_id,
+            account_id=account_id
+        )
+
+        if not have_access:
+            from fastapi import HTTPException
+            raise HTTPException(
+                status_code=403,
+                detail="Access Denied: You are not allowed to access this bank account."
+            )
+
+        users_for_account = self.user_accounts_repository.get_relation_by_account(account_id=account_id)
 
         return users_for_account
