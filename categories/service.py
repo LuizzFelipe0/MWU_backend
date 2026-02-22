27a29f66631738eeba209cf49f45dc94a46caddd
Luiz Felipe <lfcanariocosta04@gmail.com>
2026-02-22 12:58:20 -0300

    Remodel categories service with new Repository

--- categories/service.py
+++ categories/service.py
@@ -1,67 +1,64 @@
 from uuid import UUID
 
-from fastapi import Depends
+from fastapi import Depends, HTTPException
 from sqlalchemy.orm import Session
 
-from category_types.models import CategoryTypes as CategoryTypeModel
+from category_types.repository import CategoryTypeRepository
 from mwu.db import get_db
-from mwu.repositories.operational_repositories import ModelOperationalRepository
-from user.models import User as UserModel
-from .models import Category as CategoryModel
+
+from .repository import CategoryRepository
 from .schemas import CategoryInput as CategoryInScheme, CategoryUpdateInput as CategoryUpdateInScheme
 
 
-class CategoryService(ModelOperationalRepository):
+class CategoryService:
     def __init__(self, session: Session = Depends(get_db)):
-        super().__init__(model=CategoryModel, session=session)
-        self.user_service = ModelOperationalRepository(UserModel, session=session)
-        self.category_type_service = ModelOperationalRepository(CategoryTypeModel, session=session)
+        self.category_repository = CategoryRepository(session=session)
+        self.category_type_repository = CategoryTypeRepository
 
-    def get_all_categories(self):
-        categories = self.get_all()
+    def get_all_categories(self, user_id: UUID):
+        categories = self.category_repository.get_categories_by_user(user_id=user_id)
         return categories
 
-    def get_deleted_categories(self):
-        deleted_categories = self.get_all_deleted()
+    def get_deleted_categories(self, user_id: UUID):
+        deleted_categories = self.category_repository.get_deleted_categories_by_user(user_id=user_id)
         return deleted_categories
 
-    def get_category_by_id(self, id: UUID):
-        category = self.get_obj_by_id_not_deleted(obj_id=id)
+    def get_category_by_id(self, id: UUID, user_id: UUID):
+        category = self.category_repository.get_category_by_user(category_id=id, user_id=user_id)
+
         return category
 
-    def get_category_by_user(self, user_id: UUID):
-        category_by_user = self.get_objs_by_key(key="user_id", key_value=user_id)
-        return category_by_user
+    def create_category(self, data: CategoryInScheme, user_id: UUID):
+        data.user_id = user_id
 
-    def create_category(self, data: CategoryInScheme):
-        self.user_service.get_obj_by_id_not_deleted(data.user_id)
-        self.category_type_service.get_obj_by_id(data.category_type_id)
-
-        category = self.create(data=data)
+        category = self.category_repository.create(data=data)
         return category
 
-    def update_category(self, id: UUID, data: CategoryUpdateInScheme):  # Need improvement
-        category_with_id_validated = self.get_obj_by_id(id)
+    def update_category(self, id: UUID, data: CategoryUpdateInScheme,user_id: UUID):
+        self.category_repository.get_category_by_user(category_id=id, user_id=user_id)
 
-        if data.user_id is not None:
-            self.user_service.get_obj_by_id_not_deleted(data.user_id)
-        elif data.category_type_id is not None:
-            self.category_type_service.get_obj_by_id(data.category_type_id)
+        if data.category_type_id:
+            self.category_type_repository.get_by_id(data.category_type_id)
 
-        category = self.update(obj_id=category_with_id_validated.id, data=data)
+        data.user_id = user_id
+
+        category = self.category_repository.update(obj_id=id, data=data)
         return category
 
-    def delete_category(self, id: UUID):
-        category_with_id_validated = self.get_obj_by_id(id)
-        category = self.delete(obj_id=category_with_id_validated.id)
+    def delete_category(self, id: UUID, user_id: UUID):
+        category_validated = self.category_repository.get_category_by_user(category_id=id, user_id=user_id)
+
+        category = self.category_repository.soft_delete(obj_id=category_validated.id)
         return category
 
-    def restore_category(self, id: UUID):
-        category_with_id_validated = self.get_obj_by_id_deleted(id)
-        category = self.restore(obj_id=category_with_id_validated.id)
+    def restore_category(self, id: UUID, user_id: UUID):
+        deleted_category_validated = self.category_repository.get_deleted_category_by_user(category_id=id, user_id=user_id)
+
+        category = self.category_repository.restore(obj_id=deleted_category_validated.id)
         return category
 
-    def force_delete_category(self, id: UUID):
-        category_with_id_validated = self.get_obj_by_id_deleted(id)
-        category = self.force_delete(obj_id=category_with_id_validated.id)
+    def force_delete_category(self, id: UUID, user_id: UUID):
+        deleted_category_validated = self.category_repository.get_deleted_category_by_user(category_id=id, user_id=user_id)
+
+        category = self.category_repository.force_delete(obj_id=deleted_category_validated.id)
         return category
