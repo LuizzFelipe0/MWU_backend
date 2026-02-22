2e2433b28b2f396e519e0d87b6b8c13494e0dea9
Luiz Felipe <lfcanariocosta04@gmail.com>
2026-02-22 16:29:48 -0300

    Remodel user repository and service to have the same code pattern

---
+++ user/repository.py
@@ -0,0 +1,20 @@
+from uuid import UUID
+
+from sqlalchemy.orm import Session
+from mwu.repositories.base_repository import BaseRepository
+from .models import User as UserModel
+
+class UserRepository(BaseRepository):
+    def __init__(self, session: Session):
+        super().__init__(session=session, model=UserModel)
+
+    def _get_user_scoped_query(self, is_deleted: bool = False):
+        return self._get_query().filter(
+            self.model.deleted_at.isnot(None) if is_deleted else self.model.deleted_at.is_(None)
+        )
+
+    def get_all_users(self):
+        return self._get_user_scoped_query().all()
+
+    def get_all_deleted_users(self):
+        return self.db.query(self.model).filter(self.model.deleted_at.isnot(None)).all()
--- user/service.py
+++ user/service.py
@@ -5,26 +5,26 @@ from sqlalchemy.orm import Session
 
 from auth.security import is_strong_password, hash_password
 from mwu.db import get_db
-from mwu.repositories.operational_repositories import ModelOperationalRepository
-from .models import User as UserModel
+
+from .repository import UserRepository
 from .schemas import UserInput as UserInScheme, UserUpdateInput as UserUpdateInScheme
 from .utils import cpf_validator
 
 
-class UserService(ModelOperationalRepository):
+class UserService:
     def __init__(self, session: Session = Depends(get_db)):
-        super().__init__(model=UserModel, session=session)
+        self.user_repository = UserRepository(session)
 
     def get_all_users(self):
-        users = self.get_all_not_deleted()
+        users = self.user_repository.get_all()
         return users
 
     def get_deleted_users(self):
-        users = self.get_all_deleted()
+        users = self.user_repository.get_all_deleted_users()
         return users
 
     def get_user_by_id(self, id: UUID):
-        user = self.get_obj_by_id_not_deleted(obj_id=id)
+        user = self.user_repository.get_by_id(obj_id=id)
         return user
 
     def create_user(self, data: UserInScheme):
@@ -32,15 +32,19 @@ class UserService(ModelOperationalRepository):
         cpf_validator(data.cpf)
 
         hashed_password = hash_password(data.password)
+
         user_data = data.dict()
         user_data['password'] = hashed_password
         user_data['cpf'] = cpf_validator(user_data['cpf'])
+        user_data['is_admin'] = False
 
-        user = self.create(data=UserInScheme(**user_data))
+        user = self.user_repository.create(data=UserInScheme(**user_data))
+
         return user
 
     def update_user(self, id: UUID, data: UserUpdateInScheme):
-        user_with_id_validated = self.get_obj_by_id_not_deleted(id)
+        user_with_id_validated = self.user_repository.get_by_id(id)
+
         user_data = data.dict(exclude_unset=True)
 
         if 'password' in user_data:
@@ -50,20 +54,21 @@ class UserService(ModelOperationalRepository):
         if 'cpf' in user_data:
             user_data['cpf'] = cpf_validator(user_data['cpf'])
 
-        user = self.update(obj_id=user_with_id_validated.id, data=UserUpdateInScheme(**user_data))
+        user = self.user_repository.update(obj_id=user_with_id_validated.id, data=UserUpdateInScheme(**user_data))
+
         return user
 
     def delete_user(self, id: UUID):
-        user_with_id_validated = self.get_obj_by_id_not_deleted(id)
-        user = self.delete(obj_id=user_with_id_validated.id)
+        user_with_id_validated = self.user_repository.get_by_id(id)
+        user = self.user_repository.soft_delete(obj_id=user_with_id_validated.id)
         return user
 
     def restore_user(self, id: UUID):
-        user_with_id_validated = self.get_obj_by_id_deleted(id)
-        user = self.restore(obj_id=user_with_id_validated.id)
+        user_with_id_validated = self.user_repository.get_by_id(id)
+        user = self.user_repository.restore(obj_id=user_with_id_validated.id)
         return user
 
     def force_delete_user(self, id: UUID):
-        user_with_id_validated = self.get_obj_by_id_deleted(id)
-        user = self.force_delete(obj_id=user_with_id_validated.id)
+        user_with_id_validated = self.user_repository.get_by_id(id)
+        user = self.user_repository.force_delete(obj_id=user_with_id_validated.id)
         return user
