d785c55b7e9afe8691674944b7a904faa22cfdf6
Luiz Felipe <lfcanariocosta04@gmail.com>
2026-02-22 19:51:32 -0300

    Add authentication at accounts controller

--- accounts/controller.py
+++ accounts/controller.py
@@ -3,6 +3,7 @@ from uuid import UUID
 from fastapi import APIRouter, Depends
 from fastapi_utils.cbv import cbv
 
+from auth.utils import get_current_user
 from .schemas import AccountOutput as AccountOutScheme, AccountInput as AccountInScheme, \
     AccountUpdateInput as AccountUpdateInScheme
 from .service import AccountService
@@ -15,46 +16,41 @@ class AccountController:
     service: AccountService = Depends()
 
     @accounts_router.get("/all", response_model=list[AccountOutScheme | None])
-    def get_all_accounts(self):
-        accounts = self.service.get_all_accounts()
+    def get_all_accounts(self, current_user = Depends(get_current_user)):
+        accounts = self.service.get_all_accounts(user_id=current_user.id)
         return accounts
 
     @accounts_router.get("/deleted", response_model=list[AccountOutScheme | None])
-    def get_deleted_accounts(self):
-        deleted_accounts = self.service.get_deleted_accounts()
+    def get_deleted_accounts(self, current_user = Depends(get_current_user)):
+        deleted_accounts = self.service.get_deleted_accounts(user_id=current_user.id)
         return deleted_accounts
 
     @accounts_router.get("/{account_id}")
-    def get_account_by_id(self, account_id: UUID) -> AccountOutScheme | None:
-        account = self.service.get_account_by_id(id=account_id)
-        return account
-
-    @accounts_router.get("/{user_id}/user", response_model=list[AccountOutScheme | None])
-    def get_accounts_by_user(self, user_id: UUID):
-        account = self.service.get_accounts_by_user(user_id=user_id)
+    def get_account_by_id(self, account_id: UUID, current_user = Depends(get_current_user)) -> AccountOutScheme | None:
+        account = self.service.get_account_by_id(id=account_id, user_id=current_user.id)
         return account
 
     @accounts_router.post("/create", response_model=AccountOutScheme, status_code=201)
-    def create_account(self, data: AccountInScheme) -> AccountOutScheme:
-        account = self.service.create_account(data)
+    def create_account(self, data: AccountInScheme, current_user = Depends(get_current_user)) -> AccountOutScheme:
+        account = self.service.create_account(data=data, user_id=current_user.id)
         return account
 
     @accounts_router.patch("/{account_id}/update", status_code=200)
-    def update_account(self, account_id: UUID, data: AccountUpdateInScheme) -> AccountOutScheme:
-        account = self.service.update_account(id=account_id, data=data)
+    def update_account(self, account_id: UUID, data: AccountUpdateInScheme,  current_user = Depends(get_current_user)) -> AccountOutScheme:
+        account = self.service.update_account(id=account_id, data=data, user_id=current_user.id)
         return account
 
     @accounts_router.delete("/{account_id}/delete", status_code=200)
-    def delete_account(self, account_id: UUID) -> AccountOutScheme:
-        account = self.service.delete_account(id=account_id)
+    def delete_account(self, account_id: UUID, current_user = Depends(get_current_user)) -> AccountOutScheme:
+        account = self.service.delete_account(id=account_id, user_id=current_user.id)
         return account
 
     @accounts_router.post("/{account_id}/restore", status_code=200)
-    def restore_account(self, account_id: UUID) -> AccountOutScheme:
-        account = self.service.restore_account(id=account_id)
+    def restore_account(self, account_id: UUID, current_user = Depends(get_current_user)) -> AccountOutScheme:
+        account = self.service.restore_account(id=account_id, user_id=current_user.id)
         return account
 
     @accounts_router.delete("/{account_id}/force-delete", status_code=204)
-    def force_delete_account(self, account_id: UUID):
-        account = self.service.force_delete_account(id=account_id)
+    def force_delete_account(self, account_id: UUID,  current_user = Depends(get_current_user)):
+        account = self.service.force_delete_account(id=account_id, user_id=current_user.id)
         return account
