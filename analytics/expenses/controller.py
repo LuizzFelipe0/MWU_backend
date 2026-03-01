92c3e0166bdd374b1679e2ffe8fe81453aaffbb0
Luiz Felipe <lfcanariocosta04@gmail.com>
2026-03-01 13:31:56 -0300

    Remodel transaction controller to use authenticated user

--- analytics/expenses/controller.py
+++ analytics/expenses/controller.py
@@ -1,5 +1,4 @@
 from typing import Optional
-from uuid import UUID
 
 from fastapi import APIRouter, Depends, Query
 from fastapi_utils.cbv import cbv
@@ -9,21 +8,22 @@ from analytics.expenses.schemas import (
     TotalExpenseCategoryDistribution,
 )
 from analytics.expenses.service import ExpensesService
+from auth.utils import get_current_user
 
-expenses_router = APIRouter(prefix="/expenses-analysis", tags=["Expenses Analysis"])
+expenses_router = APIRouter(prefix="/expenses", tags=["Expenses Analysis"])
 
 
 @cbv(expenses_router)
 class ExpensesController:
     service: ExpensesService = Depends()
 
-    @expenses_router.get("/expenses/{user_id}/monthly-by-category", response_model=list[MonthlyCategoryExpense])
+    @expenses_router.get("/monthly/category-type", response_model=list[MonthlyCategoryExpense])
     def get_monthly_expenses_by_category_type(
             self,
-            user_id: UUID,
+            current_user = Depends(get_current_user),
             is_recurring_expense: Optional[bool] = Query(None, description="Filter by recurring expenses."),
     ):
-        return self.service.get_monthly_expenses_by_category(user_id, is_recurring_expense)
-    @expenses_router.get("/expenses/{user_id}/distribution", response_model=list[TotalExpenseCategoryDistribution])
-    def get_total_expense_distribution(self, user_id: UUID):
-        return self.service.get_total_expense_distribution(user_id)
+        return self.service.get_monthly_expenses_by_category(user_id=current_user.id, is_recurring_expense=is_recurring_expense)
+    @expenses_router.get("/distribution", response_model=list[TotalExpenseCategoryDistribution])
+    def get_total_expense_distribution(self, current_user = Depends(get_current_user)):
+        return self.service.get_total_expense_distribution(user_id=current_user.id)
