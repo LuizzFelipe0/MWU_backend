17cfe03c4910176d4cea9e037898b4c72326c6a4
Luiz Felipe <lfcanariocosta04@gmail.com>
2026-03-01 12:43:14 -0300

    Add authenticator at goals controller

--- analytics/goals/controller.py
+++ analytics/goals/controller.py
@@ -1,11 +1,10 @@
-from uuid import UUID
-
 from fastapi import APIRouter,Depends
 from fastapi_utils.cbv import cbv
 
 from analytics.goals.schemas import GoalsOutput
 
 from analytics.goals.service import GoalsService
+from auth.utils import get_current_user
 
 goals_router = APIRouter(prefix="/goals-analysis", tags=["Goals Analysis"])
 
@@ -14,7 +13,7 @@ goals_router = APIRouter(prefix="/goals-analysis", tags=["Goals Analysis"])
 class GoalsController:
     service: GoalsService = Depends()
 
-    @goals_router.get("/{user_id}", response_model=list[GoalsOutput])
-    def get_goals_to_be_reached_by_user(self, user_id: UUID):
-        goals = self.service.get_goals_to_be_reached_by_user(user_id=user_id)
+    @goals_router.get("/all", response_model=list[GoalsOutput])
+    def get_all(self, current_user = Depends(get_current_user)):
+        goals = self.service.get_goals_to_be_reached(user_id=current_user.id)
         return goals
