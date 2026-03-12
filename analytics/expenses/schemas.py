631e17abd4967c2e68d50edb27a3fcdbc7d10bc2
Luiz Felipe <lfcanariocosta04@gmail.com>
2026-03-11 21:09:27 -0300

    Add category_name at MonthlyExpense schema

--- analytics/expenses/schemas.py
+++ analytics/expenses/schemas.py
@@ -1,10 +1,11 @@
 from pydantic import BaseModel
 
 
-class MonthlyCategoryExpense(BaseModel):
+class MonthlyExpense(BaseModel):
     year: int
     month: int
     category_type_name: str
+    category_name: str
     is_positive: bool
     percentage: float
     total_amount: float
